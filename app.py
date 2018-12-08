21f4512015ffbd4da0365e5a6d9bd428b52bdb15
Johnlock1 <chris.a.christou@gmail.com>
2018-12-09 00:04:43 +0200

    config

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import os
 from flask import Flask
+
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
@@ -7,5 +11,11 @@ def hello():
     return "Hello World!"
 
 
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+
 if __name__ == '__main__':
+    print(os.environ['APP_SETTINGS'])
     app.run()
