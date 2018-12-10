f83c99593dd277208dfead4740d11cacbe8d304c
Johnlock1 <chris.a.christou@gmail.com>
2018-12-10 12:29:03 +0200

    upload & save file

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import os
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = os.path.abspath(os.path.dirname('uploads')) + '/uploads'
+ALLOWED_EXTENSIONS = set(['pdf', 'xls', 'xlsx'])
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy(app)
 
 from models import *
@@ -41,6 +46,39 @@ def importing():
     else:
         return render_template('import.html')
 
+# http://flask.pocoo.org/docs/1.0/patterns/fileuploads/
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload_file', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file parts
+        if 'file' not in request.files:
+            flash('No file was selected')
+            return redirect(request.url)
+        file = request.files['file']
+        # if the user does not select file, browser also
+        # submit and empty part without filename
+        if file.name == '':
+            flash('No selected file')
+            return redirect(request.url)
+        # if users submit file with ext. not in ALLOWED_EXTENSIONS
+        if not allowed_file(file.filename):
+            flash('File extension isn\'t allowed')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('Success! File was uploaded.')
+
+    return redirect(url_for('importing'))
+
 
 if __name__ == '__main__':
+    print(UPLOAD_FOLDER)
     app.run()
